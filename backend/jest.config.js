5f9832333e2b6d674cf03a94784bc74ccf268876
Safvan Husain <developer.safvan@gmail.com>
2025-07-09 12:43:10 +0530

    format

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,19 +2,19 @@ module.exports = {
   preset: 'ts-jest',
   moduleFileExtensions: ["js", "json", "ts"],
   rootDir: ".",
-  testRegex: "src/.*\\.spec\\.ts$",
+  testRegex: "app/.*\\.spec\\.ts$",
   // transform is usually handled by preset, but can be specified if needed
   // transform: {
   //   "^.+\\.(t|j)s$": "ts-jest",
   // },
   collectCoverageFrom: [
-    "src/**/*.service.ts",
+    "app/**/*.service.ts",
   ],
   coverageDirectory: "./coverage",
   testEnvironment: "node",
   moduleNameMapper: {
-    "^@modules/(.*)$": "<rootDir>/src/modules/$1",
-    "^@config/(.*)$": "<rootDir>/src/config/$1",
-    "^@common/(.*)$": "<rootDir>/src/common/$1",
+    "^@modules/(.*)$": "<rootDir>/app/modules/$1",
+    "^@config/(.*)$": "<rootDir>/app/config/$1",
+    "^@common/(.*)$": "<rootDir>/app/common/$1",
   },
 };
